e1b7218d7dbd35693a7667a6ef03a7067db12c92
bm <bm@basepointmedia.com>
2014-11-20 14:57:50 +0100

    simplify root path

--- app.rb
+++ app.rb
@@ -3,10 +3,5 @@ require 'sinatra/activerecord'
 require './database-config'
  
 get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Welcome."
-  erb :"posts/index"
-end
- 
-class Post < ActiveRecord::Base
+  "hello"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120134513) do
-
-  create_table "posts", force: true do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end

47d6875b095d2c5b3573dfbcc10c5818d53fafed
bm <bm@basepointmedia.com>
2014-11-20 11:48:19 +0100

    remove sqlite3 from app

--- lib/panka_app.rb
+++ lib/panka_app.rb
@@ -1,46 +1,28 @@
 require 'sinatra'
-require 'active_record'
-
-require_relative 'model/patient'
 
 class Panka < Sinatra::Base
 
-	ActiveRecord::Base.establish_connection(
-      :adapter  => 'sqlite3',
-      :database => 'spec/test.db'
-    )
-
-    ActiveRecord::Schema.define do
-      unless ActiveRecord::Base.connection.tables.include? 'patients'
-        create_table :patients do |table|
-          table.column :name, :string
-          table.column :birth_date, :date
-        end
-      end
-    end
-
-    Patient.new(:name => "Peti", :birth_date => Time.now).save
-
 	get "/patients" do
-		patients = Patient.all.to_json
+		"hello"
 	end
 
 	post "patient" do
 	end
 
 	delete "patient/:id" do |id|
-		Patient.find(id).destroy
+		"delete patient #{id}"
 	end
 
 	get "/patient/:id" do |id|
-		Patient.find(id).to_json
+		"find patient by id: #{id}"
 	end
 
 	put "patient" do
-		patient = Patient.find(params[:id])
-		unless patient == null
-			patient.update(:name => params[:name], :birth_date => params[:birth_date])
-		end
+		# patient = Patient.find(params[:id])
+		# unless patient == null
+		# 	patient.update(:name => params[:name], :birth_date => params[:birth_date])
+		# end
+		"update patient"
 	end
 
 	run! if app_file == $0
